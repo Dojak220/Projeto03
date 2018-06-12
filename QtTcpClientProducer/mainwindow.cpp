83cf92f2bb8f4c361f56e003f5e4b19bec7c4d1e
diego.diogenes <diego.diogenes@lais.huol.ufrn.br>
2018-06-12 18:11:57 -0300

    Faltando testar com o servidor

--- QtTcpClientProducer/mainwindow.cpp
+++ QtTcpClientProducer/mainwindow.cpp
@@ -6,40 +6,49 @@ MainWindow::MainWindow(QWidget *parent) :
   QMainWindow(parent), ui(new Ui::MainWindow){
   ui->setupUi(this);
   socket = new QTcpSocket(this);
-  tcpConnect();
 
   connect(ui->pushButtonConnect,
           SIGNAL(clicked(bool)),
           this,
-          SLOT(putData()));
+          SLOT(tcpConnect()));
   connect(ui->pushButtonDisconnect,
           SIGNAL(clicked(bool)),
           this,
-          SLOT(putData()));
+          SLOT(tcpDisconnect()));
   connect(ui->pushButtonStart,
           SIGNAL(clicked(bool)),
           this,
-          SLOT(putData()));
+          SLOT(initTimer()));
   connect(ui->pushButtonStop,
           SIGNAL(clicked(bool)),
           this,
-          SLOT(putData()));
+          SLOT(destroyTimer()));
   connect(ui->horizontalSliderMin,
           SIGNAL(clicked(bool)),
           this,
-          SLOT(putData()));
+          SLOT(setMin(int)));
   connect(ui->horizontalSliderMax,
           SIGNAL(clicked(bool)),
           this,
-          SLOT(putData()));
+          SLOT(setMax(int)));
   connect(ui->horizontalSliderTimings,
           SIGNAL(clicked(bool)),
           this,
-          SLOT(putData()));
+          SLOT(setTimer(int)));
+  connect(ui->setIP,
+          SIGNAL(textChanged(QString)),
+          this,
+          SLOT(mudarIP(QString)));
+
+
+  endereco_ip = "127.0.0.1";
+  timer = 1;
+  min = 1;
+  max = 10;
 }
 
 void MainWindow::tcpConnect(){
-  socket->connectToHost("127.0.0.1",1234);
+  socket->connectToHost(endereco_ip,1234);
   if(socket->waitForConnected(3000)){
     qDebug() << "Connected";
   }
@@ -64,6 +73,63 @@ void MainWindow::putData(){
         qDebug() << "wrote";
       }
   }
+}
+
+void MainWindow::tcpDisconnect()
+{
+    socket->disconnectFromHost();
+    if(socket->waitForConnected(3000)){
+      qDebug() << "Connected";
+    }
+    else{
+      qDebug() << "Disconnected";
+    }
+}
+
+void MainWindow::mudarIP(QString _ip)
+{
+    endereco_ip = QString (_ip);
+}
+
+void MainWindow::setMin(int _min)
+{
+    if(max <= _min){
+        ui->horizontalSliderMax->setValue(_min);
+    }
+    min = _min;
+}
+
+void MainWindow::setMax(int _max)
+{
+    if(min >= _max){
+        ui->horizontalSliderMin->setValue(_max);
+    }
+    max = _max;
+}
+
+void MainWindow::initTimer()
+{
+    if(timers.size()==0){
+        timers.push_back(startTimer(1000*timer));
+    }
+    else{
+        killTimer(timers[0]);
+        timers[0] = startTimer(1000*timer);
+    }
+}
+
+void MainWindow::destroyTimer()
+{
+    killTimer(timers[0]);
+    timers.clear();
+}
+
+void MainWindow::setTimer(int _t)
+{
+    timer = _t;
+    if(timers.size()){
+        initTimer();
+    }
 }
 
 MainWindow::~MainWindow(){
--- QtTcpClientProducer/mainwindow.h
+++ QtTcpClientProducer/mainwindow.h
@@ -4,6 +4,8 @@
 #include <QMainWindow>
 #include <QTcpSocket>
 #include <QDebug>
+#include <QTimerEvent>
+#include <vector>
 
 namespace Ui {
 class MainWindow;
@@ -12,7 +14,9 @@ class MainWindow;
 class MainWindow : public QMainWindow
 {
   Q_OBJECT
-
+  int min, max, timer;
+  std::vector <int>timers;
+  QString endereco_ip;
 public:
   explicit MainWindow(QWidget *parent = 0);
   ~MainWindow();
@@ -20,6 +24,15 @@ public:
   void tcpConnect();
 public slots:
   void putData();
+  void tcpDisconnect();
+  void mudarIP(QString _ip);
+  void setMin(int _min);
+  void setMax(int _max);
+
+  void initTimer();
+  void destroyTimer();
+  void setTimer(int _t);
+
 private:
   Ui::MainWindow *ui;
   QTcpSocket *socket;
